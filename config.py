7a1deb413dea9c48f4fd4ba4e9bd52c771ec038b
praptigawade12-sys <praptigawade12@gmail.com>
2026-05-08 16:59:26 +0400

    v2

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
 import os
 import streamlit as st
+
+# For local .env usage
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_API_KEY = os.getenv("HF_API_KEY")
+
+# For Streamlit Cloud secrets support
+if not GROQ_API_KEY:
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", None)
+
+if not HF_API_KEY:
+    HF_API_KEY = st.secrets.get("HF_API_KEY", None)
 
 if not GROQ_API_KEY:
-  raise ValueError("GROQ_API_KEY is missing")
+    raise ValueError("GROQ_API_KEY is missing")
+
+GROQ_MODELS = [
+    "llama-3.3-70b-versatile"
+]
+
+HF_MODELS = [
+    "meta-llama/Llama-3.1-8B-Instruct"
+]
